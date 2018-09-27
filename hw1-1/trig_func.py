58f17537ab558bd702d056a40da5e352b6633a98
jeffeuxMartin <43311603+jeffeuxMartin@users.noreply.github.com>
2018-09-27 23:42:18 +0800

    Add files via upload

--- hw1-1/trig_func.py
+++ hw1-1/trig_func.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 25 19:07:14 2018
+Created on Wed Sep 26 05:42:22 2018
 
 @author: jeffchen
 """
@@ -13,13 +13,18 @@ import pylab as pl
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
+# Generating triangular wave
 X0 = np.linspace(-4.0 * np.pi, 4.0 * np.pi, 2000).reshape(-1, 1)
-Y0 = np.sin(np.pi * X0) / (np.pi * X0) # since X is never 0 here.
-np.random.seed(7)
+X00 = list(X0.reshape(1,-1)[0])
+Y0 = [0]*len(X00)
+freq = 4.0
+for j in range(len(X00)):
+	Y0[j] = abs(X00[j]%freq-freq/2.0)/(freq/4.0)-1
+Y0 = np.array(Y0).reshape(-1, 1)
 
+# Rescale X and Y
 X_scaler = MinMaxScaler(feature_range=(-1.0, 1.0))
 Y_scaler = MinMaxScaler(feature_range=(-1.0, 1.0))
-
 X = X_scaler.fit_transform(X0)
 Y = Y_scaler.fit_transform(Y0)
 
@@ -31,14 +36,13 @@ color_buffer = ['ro', 'yo', 'bo']
 tries = 3
 
 for j in range(tries):
-	epochs_tried, batches = 20000, 32
+	epochs_tried, batches = 1000, 32
 	if j == 0:
-		neurons = [25,25,10,20,100,10,90,50,5,20,10,100,10,25]
+		neurons = [51,200,80,3,9]
 	if j == 1:
-		neurons = [5,2,4,8,20,5,40,25,40,100,25,20,50,20,35]
+		neurons = [124,25,20,12,20,40,25,110,90,50,25,56]
 	if j == 2:
-		neurons = [19,15,10,10,70,20,80,50,20,20,30,50,10,10,5]
-
+		neurons = [12,30,20,25,20,80,10,40,30,100,100,25,100,20,13]
 
 	model = Sequential()
 	model.add(Dense(units=neurons[0], activation='relu', input_dim=X.shape[1]))
