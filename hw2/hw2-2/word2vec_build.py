fe0a25af44d47b402804c697db9e7ed89723b69f
jeffeuxMartin <43311603+jeffeuxMartin@users.noreply.github.com>
2018-11-09 21:18:29 +0800

    Update Word2Vec and including <UNK> and <EOS>
    Complete code of Word2Vec, but one-hot not included.
    (Uploader: Jeff Chen)

--- hw2/hw2-2/word2vec_build.py
+++ hw2/hw2-2/word2vec_build.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 29 02:44:57 2018
-First created on Sat Oct 27 17:35:25 2018
+Created         on Sun Oct 29 02:44:57 2018
+First created   on Sat Oct 27 17:35:25 2018
+Second modified on Fri Nov  9 17:15:44 2018
+Last modified   on Fri Nov  9 17:48:49 2018
 
 @author: Jeff Chen
 """
@@ -9,12 +11,18 @@ First created on Sat Oct 27 17:35:25 2018
 from gensim.test.utils import common_texts, get_tmpfile
 from gensim.models import Word2Vec
 import numpy as np
-#import matplotlib.pyplot as plt
 import codecs
+from collections import defaultdict
+from pprint import pprint
 
 directory = "/home/jovyan/ntulee/b05901009/hw2-2/mlds_hw2_2_data/"
+unknown_threshold = 5
+iterations = 10
+print(">>> current directory: \"{}\"<<<".format(directory))
+print("### PROCESS  STARTED ###")
 
-# Sec. 1: Loading coversation
+# Sec. 1: Loading conversations
+print("Loading conversations...")
 with open(directory + "clr_conversation.txt", "r") as f:
     sentences = [dialogue.split('\n') for dialogue in f.read().split('+++$+++')]
 contents = []
@@ -25,16 +33,31 @@ for i in sentences:
         i = i[:-1]
     contents.extend(i)
 contents = [sentence.split() for sentence in contents]
-print("Start training model")
-# Sec. 2: Generating models
-# dictionary = gensim.corpora.Dictionary(contents)
-# path = get_tmpfile("word2vec.model")
-model = Word2Vec(contents, size=250, window=5, min_count=3, workers=4)
-model.save(directory + "word2vec_run_by_Wei_Tsung.model")
-# model_try.train(["我們", "該", "讓", "他們", "回到", "家鄉"], total_examples=1, epochs=1)
+
+print("Adding <UNK> and <EOS>...")
+# Sec. 2: Low frequent words elimination and attachment of sentence endings
+freq = defaultdict(int)
+for text in contents:
+    for token in text:
+        freq[token] += 1
+texts = []
+for sent in contents:
+    _sent = []
+    for token in sent:
+        if not freq[token] >= unknown_threshold:
+            _sent.append("<UNK>")
+        else:
+            _sent.append(token)
+    texts.append(_sent + ['<EOS>'])
+contents = texts
+del texts, _sent
+
+print("Start training...")
+# Sec. 3: Generating models
+model = Word2Vec(contents, size=250, window=5, min_count=unknown_threshold, workers=4, iter=iterations)
+print("Saving the model.")
+model.save("./word2vec_run_by_Jeff.model")
 vocab = list(dict(model.wv.vocab.items()).keys())
-print('The size of vocabulary =', len(vocab))
-# for j in vocab:
-#     print(j, model.wv[j])
-# modelp = Word2Vec.load(directory + "word2vec.model")
-# modelp.wv['say']
+print('The size of vocabulary =', len(vocab), ".")
+
+print("###PROCESS TERMINATED###")
